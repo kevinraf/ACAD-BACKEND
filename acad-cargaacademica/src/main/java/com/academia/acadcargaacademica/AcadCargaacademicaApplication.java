8c96b61de6020d1a74c6aa9f45bc6892b015f2ed
KEVINRAF <paricahuak40@gmail.com>
2025-07-01 21:41:34 -0500

    cargaAcademica

---
+++ acad-cargaacademica/src/main/java/com/academia/acadcargaacademica/AcadCargaacademicaApplication.java
@@ -0,0 +1,13 @@
+package com.academia.acadcargaacademica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadCargaacademicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcadCargaacademicaApplication.class, args);
+    }
+
+}
---
+++ acad-cargaacademica/src/test/java/com/academia/acadcargaacademica/AcadCargaacademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.acadcargaacademica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadCargaacademicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
