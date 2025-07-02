4f2245d76e38805fb46ca3c0b271cadf5fbdbe0f
KEVINRAF <paricahuak40@gmail.com>
2025-07-02 00:20:52 -0500

    cargaAcademica interno2

--- acad-cargaacademica/src/main/java/com/academia/acadcargaacademica/Controlador/DocenteControlador.java
+++ acad-cargaacademica/src/main/java/com/academia/acadcargaacademica/Controlador/DocenteControlador.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 @RestController
-@RequestMapping("/apoderados")
+@RequestMapping("/docentes")
 public class DocenteControlador {
     @Autowired
     private DocenteServicio servicio;
